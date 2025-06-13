32398708b30becb97d0f8cffff228b9d9574852b
chiaki187 <23fi094@ms.dendai.ac.jp>
2025-06-13 23:20:49 +0900

    d

--- main.py
+++ main.py
@@ -61,3 +61,7 @@ def index():
 @app.post("/subject")
 async def give_subject(subject):
     return {"response": f"サーバです。 {subject}の試験お疲れ様。結果は100点満点だよ！"}  # f文字列というPythonの機能を使っている
+
+@app.post("/subject")  # ここを変更
+async def give_subject(subject):
+    return {"response": f"サーバです。{subject}の試験お疲れ様。結果は100点満点だよ！"}
