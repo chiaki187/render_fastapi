0c018eabd3939b477107cd7fcba008c96277d597
chiaki187 <23fi094@ms.dendai.ac.jp>
2025-06-13 12:00:09 +0900

    3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/omikuji")
+@app.get("/omikuji3")
 def omikuji():
     omikuji_list = [
         "大吉",
